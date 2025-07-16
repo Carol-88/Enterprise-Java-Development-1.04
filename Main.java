305f734611c89bcf455222edcc439f1b1a383b5e
Carol-88 <gralleta@gmail.com>
2025-07-16 14:37:38 +0200

    task1 done

---
+++ Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.example.task1.Task1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // TASK 1
+        int[] miArray = {1, 5, 3, 11, 15, 100};
+        int resultado = Task1.calcularDiferencia(miArray);
+
+        System.out.println("La diferencia es: " + resultado);
+
+        // TASK 2
+
+        // TASK 3
+
+    }
+
+    /*
+    1. Generar los números
+    2. Definir la función
+        1. Recibir un array
+        2. Select min-max
+        3. Iterar sobre el array comprobando min-max
+        4. Realizar la resta
+        5. Devolver la resta
+     3. Mostrar en pantalla el resultado.
+     */
+}
---
+++ task1/Task1.java
@@ -0,0 +1,34 @@
+package org.example.task1;
+
+public class Task1 {
+    public static int calcularDiferencia(int[] array) {
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return max - min;
+    }
+}
+
+
+
+//La clave para encontrar el mínimo y el máximo es usar una estrategia sencilla:
+//Empezar con una suposición.
+//Antes de empezar a recorrer el array, puedes suponer que el primer elemento es,
+//al mismo tiempo, el valor más pequeño y el más grande.
+//Crea dos variables, por ejemplo minimo y maximo, y asígnales el valor del primer elemento
+//del array (array[0]). Esto es seguro porque el problema dice que el array siempre tendrá
+//al menos un elemento. Recorrer el array y comparar. Después de inicializar tus variables,
+//usa un bucle (for) para recorrer el resto del array (desde el segundo elemento, array[1],
+//hasta el final).
+//En cada paso del bucle, haz estas dos preguntas: ¿Es el elemento actual más grande que maximo?
+//Si es así, actualiza maximo con el valor de ese elemento. ¿Es el elemento actual más
+//pequeño que minimo? Si es así, actualiza minimo con el valor de ese elemento.
